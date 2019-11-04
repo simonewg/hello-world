29f5a91bac55f13081a19622fbc66ecd7ef890e3
simonewg <simonewg@users.noreply.github.com>
2019-11-04 13:05:07 +0100

    commit3
    primo programma

---
+++ helloworld/Model/person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helloworld.Model
+{
+    class Person
+    {
+        string nome;
+        string cognome;
+        public int eta;
+        string sesso;
+        int altezza;
+        double peso;
+
+        public Person(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.sesso = "sconosciuto";
+
+        }
+
+     
+        public string Anagrafica()
+            {
+            string anagrafica = this.nome + this.cognome;
+            return anagrafica;
+
+               
+            }
+        
+
+
+    }
+}
--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using helloworld.Model;
+using System;
 
 namespace helloworld
 {
@@ -6,7 +7,47 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ciao mondo!");
+            //value type
+
+            int Intero;
+            float NumeroConLaVirgola32;
+            double NumeroConLaVirgola64;
+            bool ValoreLogico;
+            char car;
+            decimal Decimale; //valore preciso con operazioni matematiche 
+
+            int i = 5;
+            int j = 10;
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+           
+            j = i;
+            
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+
+            //reference type
+
+            string InsiemeDiCaratteri;
+            string stringa1 = "la mia casa e bella";
+            string stringa2 = stringa1;
+
+            Console.WriteLine(stringa1);
+            Console.WriteLine(stringa2);
+            
+          
+
+
+            object oggetto;
+            
+             Person simone = new Person("simone", "gaudiano");
+             simone.eta = 18;
+
+            Console.WriteLine(simone.Anagrafica());
+
+
         }
     }
 }
